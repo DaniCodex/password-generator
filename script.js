1a7f0d8d3191420d643e8e521983238f29041ba2
Mike <revolutiondaniel19@gmail.com>
2023-07-15 21:05:29 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const password = document.getElementById("pass"),
+      copy = document.getElementById("copy"),
+      generatePassword = document.getElementById("generatePass"),
+      length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      lowerCase = "abcdefghijklmnopqrstuvwxyz",
+      number = "0123456789",
+      symbol = "@#$%^&*()_+~|}{[]></-=",
+      allChars = upperCase + lowerCase + number + symbol;
+    
+
+
+
+function generatePass() {
+    
+    
+}
+
+function copyPass() {
+    
+}
